f708e899de1197fce7b20570801e647b43420dec
Craig Swanson <craig@piso13.com>
2020-12-02 22:37:59 -0600

    Initial implementation of cpu (day 1)

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,8 +7,11 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        self.ram = []
-        pass
+        self.ram = [0] * 256
+        self.reg = [0] * 8
+        self.pc = 0
+
+        self.reg[7] = 0xf4
 
     def load(self):
         """Load a program into memory."""
@@ -41,6 +44,29 @@ class CPU:
         else:
             raise Exception("Unsupported ALU operation")
 
+    def hlt(self):
+        exit()
+
+    def ldi(self, register, immediate):
+        self.reg[register] = immediate
+        return
+    
+    def prn(self, register):
+        binary_string = str(self.reg[register])
+        # int_value = (int(binary_string, 2))
+        # print(int_value)
+        print(binary_string)
+        return
+    
+    def ram_read(self, address_to_read):
+        # return value stored at address_to_read
+        return self.ram[address_to_read]
+
+    def ram_write(self, value_to_write, address_to_write):
+        # store value_to_write at address_to_write
+        self.ram[address_to_write] = value_to_write
+        return
+
     def trace(self):
         """
         Handy function to print out the CPU state. You might want to call this
@@ -63,4 +89,32 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        ir = 0 # a copy of the currently executing instruction
+        operand_a = 0
+        operand_b = 0
+        # read the address in pc and store the result in ir.
+        # using ram_read(), read PC+1 and PC+2 from RAM into
+        # the variable operand_a and operand_b
+        # then depending on the value of opcode, perform action.
+
+        running = True
+
+        while running:
+            # self.trace()
+            instruction = self.ram[self.pc]
+
+            if instruction == 0b00000001:
+                self.hlt()
+
+            elif instruction == 0b10000010:
+                ir = instruction
+                operand_a = self.ram[self.pc + 1]
+                operand_b = self.ram[self.pc + 2]
+                self.ldi(operand_a, operand_b)
+                self.pc += 3
+            
+            elif instruction == 0b01000111:
+                ir = instruction
+                operand_a = self.ram[self.pc + 1]
+                self.prn(operand_a)
+                self.pc +=2
